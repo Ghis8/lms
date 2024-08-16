afbbb7a00409e31397479129c24c11ae2d7b2518
ghis8 <ghislainkongolo@yahoo.com>
2024-08-16 19:14:42 +0200

    working on the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,19 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers/confetti-provider'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Learning Management System',
+  icons:{
+    icon:{
+      url:'/img/LMS.png',
+      rel:'icon',
+      type:'image/png'
+    }
+  },
   description: 'A Platform for Online learning',
 }
 
@@ -21,7 +29,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ConfettiProvider />
+            <ConfettiProvider />
             <ToastProvider />
               {children}
             </body>
